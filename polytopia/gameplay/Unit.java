35142e9fb3c129481dc623ae9554e8d1a68e10b0
Aaronlosl <71696212+Aaronlosl@users.noreply.github.com>
2022-05-31 20:52:54 +0800

    Update Unit.java

--- polytopia/gameplay/Unit.java
+++ polytopia/gameplay/Unit.java
@@ -16,17 +16,34 @@ public class Unit implements Visualizable {
 		NONE,
 		ARCHER, BATTLESHIP, BOAT, CATAPULT, DEFENDER, MINDBENDER, RIDER, SHIP, SWORDSMAN, WARRIOR;
 	}
+	public enum ShellType {
+		NONE,
+		SHIP, BOAT, BATTLESHIP;
+	}
 	// Attributes
 	private CharType type = CharType.NONE;
+	private ShellType shellType = ShellType.NONE;
 	private CharType carryType = CharType.NONE;
+	//
 	private int HpMax;
+	private int shellHPM;
 	private int Hp;
+	private int shellHp;
+	//
 	private int Cost;
+	private int shellCost;
+	//
 	private int AttackPower;
+	private int shellATP;
+	//
 	private int DefendPower;
+	private int shellDFP;
+	//
 	private int MoveRange;
+	private int shellMR;
+	//
 	private int AttackRange;
-	private boolean Navy;
+	private int shellAR;
 	// Skills
 	public boolean Dash;
 	public boolean Fortify;
@@ -40,7 +57,6 @@ public class Unit implements Visualizable {
 	public int recover = 1;
 	public Tile position;
 	private TileMap map;
-	private boolean roundEnd = false;
 
 	// Selection response
 	public void visualize(/* GUI component */) {
@@ -50,67 +66,71 @@ public class Unit implements Visualizable {
 	public Player getOwnerPlayer() {return this.ownerPlayer;}
 	public City getOwnerCity() {return this.ownerCity;}
 
-	Unit(Player ownerPlayer_,City ownerCity_, CharType type_) {
+	Unit(TileMap map_, Player ownerPlayer_, City ownerCity_, CharType type_) {
+		this.map = map_;
 		this.ownerPlayer = ownerPlayer_;
 		this.ownerCity = ownerCity_;
 		this.type = type_;
 		switch (type) {
 			case ARCHER: // 	dash	fortify
 				Hp = HpMax = 10;
-				Cost = 3; 	AttackPower = 2; 	DefendPower = 1;	MoveRange = 1;	AttackRange = 2;	Navy = false;
+				Cost = 3; 	AttackPower = 2; 	DefendPower = 1;	MoveRange = 1;	AttackRange = 2;
 				Dash = true;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
+				/*
 			case BATTLESHIP: // dash	scout(S)
 				Hp = HpMax = 0;
-				Cost = 15; 	AttackPower = 3; 	DefendPower = 3;	MoveRange = 3;	AttackRange = 3;	Navy = true;
+				Cost = 15; 	AttackPower = 3; 	DefendPower = 3;	MoveRange = 3;	AttackRange = 3;
 				Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = true;
 				break;
 			case BOAT: // 		-
 				Hp = HpMax = 0;
-				Cost = 0; 	AttackPower = 0;	DefendPower = 1;	MoveRange = 2;	AttackRange = 0;	Navy = true;
+				Cost = 0; 	AttackPower = 0;	DefendPower = 1;	MoveRange = 2;	AttackRange = 0;
 				Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
+				 */
 			case CATAPULT: // 	-
 				Hp = HpMax = 10;
-				Cost = 8; 	AttackPower = 4;	DefendPower = 0;	MoveRange = 1;	AttackRange = 3;	Navy = false;
+				Cost = 8; 	AttackPower = 4;	DefendPower = 0;	MoveRange = 1;	AttackRange = 3;
 				Dash = false;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
 			case DEFENDER: // 	fortify
 				Hp = HpMax = 15;
-				Cost = 3; 	AttackPower = 1;	DefendPower = 3;	MoveRange = 1;	AttackRange = 1;	Navy = false;
+				Cost = 3; 	AttackPower = 1;	DefendPower = 3;	MoveRange = 1;	AttackRange = 1;
 				Dash = false;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
 			case MINDBENDER: //	heal	convert		(detect)
 				Hp = HpMax = 10;
-				Cost = 5; 	AttackPower = 0;	DefendPower = 1;	MoveRange = 1;	AttackRange = 1;	Navy = false;
+				Cost = 5; 	AttackPower = 0;	DefendPower = 1;	MoveRange = 1;	AttackRange = 1;
 				Dash = false;	Fortify = false;Escape = false;	Heal = true;	Convert = true;		Scout = false;
 				break;
 			case RIDER: // 		dash	escape		fortify
 				Hp = HpMax = 10;
-				Cost = 3; 	AttackPower = 2;	DefendPower = 1;	MoveRange = 2;	AttackRange = 1;	Navy = false;
+				Cost = 3; 	AttackPower = 2;	DefendPower = 1;	MoveRange = 2;	AttackRange = 1;
 				Dash = true;	Fortify = true;	Escape = true;	Heal = false;	Convert = false;	Scout = false;
 				break;
+				/*
 			case SHIP: //		dash
 				Hp = HpMax = 0;
-				Cost = 5; 	AttackPower = 2;	DefendPower = 2;	MoveRange = 3;	AttackRange = 2;	Navy = true;
+				Cost = 5; 	AttackPower = 2;	DefendPower = 2;	MoveRange = 3;	AttackRange = 2;
 				Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
+				 */
 			case SWORDSMAN: //	dash	fortify
 				Hp = HpMax = 15;
-				Cost = 5; 	AttackPower = 3;	DefendPower = 3;	MoveRange = 1;	AttackRange = 1;	Navy = false;
+				Cost = 5; 	AttackPower = 3;	DefendPower = 3;	MoveRange = 1;	AttackRange = 1;
 				Dash = true;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
 			case WARRIOR: //	dash	fortify
 				Hp = HpMax = 10;
-				Cost = 2; 	AttackPower = 2;	DefendPower = 2;	MoveRange = 1;	AttackRange = 1;	Navy = false;
+				Cost = 2; 	AttackPower = 2;	DefendPower = 2;	MoveRange = 1;	AttackRange = 1;
 				Dash = true;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
 			case NONE:
 				Hp = HpMax = Cost = AttackPower = DefendPower = MoveRange = AttackRange = 0;
-				Navy = Dash = Fortify =	Escape = Heal = Convert = Scout = false;
+				Dash = Fortify =	Escape = Heal = Convert = Scout = false;
 		}
 	}
-
 	public void visualizeAttack(Tile from, Tile to) {
 		/* TODO: GUI Visualize Attack, Move,  */
 	}
@@ -118,13 +138,45 @@ public class Unit implements Visualizable {
 	public void visualizeHeal(Tile at) {}
 	public void visualizeConvert(Tile at) {}
 
+	public int getHpMax() {
+		if(shellType == ShellType.NONE) return HpMax;
+		return shellHPM;
+	}
+	public int getHp() {
+		if(shellType == ShellType.NONE) return Hp;
+		return shellHp;
+	}
+	public void setHp(int x) {
+		if(shellType == ShellType.NONE) {Hp = x;return;}
+		shellHp = x;
+	}
+	public int getCost() {
+		if(shellType == ShellType.NONE) return Cost;
+		return shellCost;
+	}
+	public int getAttackPower() {
+		if(shellType == ShellType.NONE) return AttackPower;
+		return shellATP;
+	}
+	public int getDefendPower() {
+		if(shellType == ShellType.NONE) return DefendPower;
+		return shellDFP;
+	}
+	public int getAttackRange() {
+		if(shellType == ShellType.NONE) return AttackRange;
+		return shellAR;
+	}
+	public int getMoveRange() {
+		if(shellType == ShellType.NONE) return MoveRange;
+		return shellMR;
+	}
 	public ArrayList<Tile> getSurroundings(int layer) {
 		ArrayList<Tile> surroundings = new ArrayList<>();
 		for(int i = 0; i < map.GetSize(); i++)
 			for(int j = 0; j < map.GetSize(); j++)
 				if(map.getDistance(position, map.getGrid()[i][j]) == layer)
 					// Navy then all accessibility, NonNavy then landscape and port in range are accessible
-					if(Navy || (!Navy && (map.getGrid()[i][j].getTerrainType() == Tile.TerrainType.FIELD
+					if(shellType != ShellType.NONE || (shellType == ShellType.NONE && (map.getGrid()[i][j].getTerrainType() == Tile.TerrainType.FIELD
 							|| map.getGrid()[i][j].getTerrainType() == Tile.TerrainType.FOREST
 							|| (map.getGrid()[i][j].getTerrainType() == Tile.TerrainType.MOUNTAIN
 										&& ownerPlayer.getTechs().contains(Player.Tech.CLIMBING)
@@ -135,9 +187,18 @@ public class Unit implements Visualizable {
 		return surroundings;
 	}
 
+	public ArrayList<Tile> getMovable() {
+		ArrayList<Tile> destination = new ArrayList<>();
+		for(int i = 1; i <= getMoveRange(); i++)
+			for(Tile tile: getSurroundings(i))
+				if(!tile.hasAlly(ownerPlayer) && !tile.hasEnemy(ownerPlayer))
+					destination.add(tile);
+		return destination;
+	}
+
 	public ArrayList<Tile> searchEnemy() {
 		ArrayList<Tile> accessibleEnemy = new ArrayList<>();
-		for(int i = 1; i <= AttackRange; i++)
+		for(int i = 1; i <= getAttackRange(); i++)
 			for(Tile tile: getSurroundings(i))
 				if (tile.hasEnemy(ownerPlayer))
 					accessibleEnemy.add(tile);
@@ -172,13 +233,11 @@ public class Unit implements Visualizable {
 	public void retaliation(Tile enemyTile, int defendResult) {
 		if(enemyTile.getUnit().searchEnemy().contains(position)) {
 			visualizeAttack(enemyTile, position);
-			if(defendResult >= Hp) {
+			if(defendResult >= getHp()) {
 				position.setUnit(null);
 				this.type = CharType.NONE;
 				return;
-			} else {
-				Hp -= defendResult;
-			}
+			} else {setHp(getHp() - defendResult);}
 		}
 	}
 
@@ -193,14 +252,14 @@ public class Unit implements Visualizable {
 		}
 	}
 	// Automatically for units that does nothing
-	public boolean recoverable() {return Hp < HpMax;}
+	public boolean recoverable() {return getHp() < getHpMax();}
 	public void Recover() {
-		if((Hp == HpMax) || (movable == 0)) return;
+		if((getHp() == getHpMax()) || (movable == 0)) return;
 
 		recover--;
 		movable = 0;
 		attack = 0;
-		Hp = Math.max(HpMax, Hp + 2 * getRecoveryRate(this));
+		setHp(Math.max(getHpMax(), getHp() + 2 * getRecoveryRate(this)));
 		visualizeHeal(position);
 	}
 	// Move to accessible tiles and set
@@ -210,7 +269,7 @@ public class Unit implements Visualizable {
 		if(movable == -1 ||(movable == 0 && !(attack == 0 && Escape)) || targetTile.hasAlly(ownerPlayer)
 				|| targetTile.hasEnemy(ownerPlayer)) return;
 		// Exit if not in range
-		for(int i = 1; i <= MoveRange; i++)
+		for(int i = 1; i <= getMoveRange(); i++)
 			if(!this.getSurroundings(i).contains(targetTile))
 				return;
 
@@ -219,8 +278,8 @@ public class Unit implements Visualizable {
 		visualizeMove(position, targetTile);
 		position = targetTile;
 		targetTile.setUnit(this);
-		Carry();
-		Land();
+		// Carry();
+		// Land();
 	}
 	// Attack on accessible enemies and set
 	public void Attack(Tile enemyTile) {
@@ -236,95 +295,55 @@ public class Unit implements Visualizable {
 		// Calculate on formula:  https://frothfrenzy.github.io/polytopiacalculator/?short=
 		Unit defender = enemyTile.getUnit();
 		float accelerator = 4.5F;
-		float defenseBonus = getDefenseBonus(enemyTile.getUnit());
-		float attackForce = AttackPower * (Hp / HpMax);
-		float defenseForce = DefendPower * (defender.Hp / defender.HpMax) * defenseBonus;
+		float defenseBonus = getDefenseBonus(defender);
+		float attackForce = getAttackPower() * (getHp() / getHpMax());
+		float defenseForce = getDefendPower() * (defender.getHp() / defender.getHpMax()) * defenseBonus;
 		float totalDamage = attackForce + defenseForce;
-		int attackResult = Math.round((attackForce / totalDamage) * AttackPower * accelerator);
+		int attackResult = Math.round((attackForce / totalDamage) * getAttackPower() * accelerator);
 		// Exit if enemy is killed
-		if(attackResult >= defender.Hp) {
+		if(attackResult >= defender.getHp()) {
 			kill(enemyTile);
 			return;
 		}
-		enemyTile.getUnit().Hp -= attackResult;
+		defender.setHp(defender.getHp() - attackResult);
 		// Enemy defends
-		int defenseResult = Math.round((defenseForce / totalDamage) * defender.DefendPower * accelerator);
+		int defenseResult = Math.round((defenseForce / totalDamage) * defender.getDefendPower() * accelerator);
 		retaliation(enemyTile, defenseResult);
 	}
 
 	public void Carry() {
 		// Exit if position is not a port
 		if(((Improvement)position.getVariation()).getImprovementType() != Improvement.ImprovementType.PORT) return;
-		// Transform
-		carryType = type;
-		type = CharType.BOAT;
-		AttackPower = 0;	DefendPower = 1;	MoveRange = 2;	AttackRange = 0;	Navy = true;
+		// Put on shell
+		shellType = ShellType.BOAT;
+		shellATP = 0;	shellDFP = 1;	shellMR = 2;	shellAR = 0;
 		Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
 	}
 
 	public void Land() {
-		// Exit if position is not land or type is not a boat
-		if((type != CharType.BOAT && type != CharType.BATTLESHIP && type != CharType.SHIP)
-				|| position.getTerrainType() == Tile.TerrainType.OCEAN
-				|| position.getTerrainType() == Tile.TerrainType.SHORE)
-			return;
-		// Transform
-		type = carryType;
-		carryType = CharType.NONE;
+		// Exit if not in a type of ship
+		// Exit if not in sea
+		if(shellType == ShellType.NONE || (position.getTerrainType() != Tile.TerrainType.OCEAN
+				&& position.getTerrainType() != Tile.TerrainType.SHORE)) return;
+		// Remove shell
+		shellType = ShellType.NONE;
 		switch (type) {
-			case ARCHER: // 	dash	fortify
-				Hp = HpMax = 10;
-				Cost = 3; 	AttackPower = 2; 	DefendPower = 1;	MoveRange = 1;	AttackRange = 2;	Navy = false;
+			case ARCHER:
+			case SWORDSMAN:
+			case WARRIOR:
 				Dash = true;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
-			case BATTLESHIP: // dash	scout(S)
-				Hp = HpMax = 0;
-				Cost = 15; 	AttackPower = 3; 	DefendPower = 3;	MoveRange = 3;	AttackRange = 3;	Navy = true;
-				Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = true;
-				break;
-			case BOAT: // 		-
-				Hp = HpMax = 0;
-				Cost = 0; 	AttackPower = 0;	DefendPower = 1;	MoveRange = 2;	AttackRange = 0;	Navy = true;
-				Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
-				break;
-			case CATAPULT: // 	-
-				Hp = HpMax = 10;
-				Cost = 8; 	AttackPower = 4;	DefendPower = 0;	MoveRange = 1;	AttackRange = 3;	Navy = false;
+			case CATAPULT:
 				Dash = false;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
-			case DEFENDER: // 	fortify
-				Hp = HpMax = 15;
-				Cost = 3; 	AttackPower = 1;	DefendPower = 3;	MoveRange = 1;	AttackRange = 1;	Navy = false;
+			case DEFENDER:
 				Dash = false;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
 				break;
-			case MINDBENDER: //	heal	convert		(detect)
-				Hp = HpMax = 10;
-				Cost = 5; 	AttackPower = 0;	DefendPower = 1;	MoveRange = 1;	AttackRange = 1;	Navy = false;
+			case MINDBENDER:
 				Dash = false;	Fortify = false;Escape = false;	Heal = true;	Convert = true;		Scout = false;
 				break;
-			case RIDER: // 		dash	escape		fortify
-				Hp = HpMax = 10;
-				Cost = 3; 	AttackPower = 2;	DefendPower = 1;	MoveRange = 2;	AttackRange = 1;	Navy = false;
+			case RIDER:
 				Dash = true;	Fortify = true;	Escape = true;	Heal = false;	Convert = false;	Scout = false;
-				break;
-			case SHIP: //		dash
-				Hp = HpMax = 0;
-				Cost = 5; 	AttackPower = 2;	DefendPower = 2;	MoveRange = 3;	AttackRange = 2;	Navy = true;
-				Dash = true;	Fortify = false;Escape = false;	Heal = false;	Convert = false;	Scout = false;
-				break;
-			case SWORDSMAN: //	dash	fortify
-				Hp = HpMax = 15;
-				Cost = 5; 	AttackPower = 3;	DefendPower = 3;	MoveRange = 1;	AttackRange = 1;	Navy = false;
-				Dash = true;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
-				break;
-			case WARRIOR: //	dash	fortify
-				Hp = HpMax = 10;
-				Cost = 2; 	AttackPower = 2;	DefendPower = 2;	MoveRange = 1;	AttackRange = 1;	Navy = false;
-				Dash = true;	Fortify = true;	Escape = false;	Heal = false;	Convert = false;	Scout = false;
-				break;
-			case NONE:
-				Hp = HpMax = Cost = AttackPower = DefendPower = MoveRange = AttackRange = 0;
-				Navy = Dash = Fortify =	Escape = Heal = Convert = Scout = false;
 		}
 	}
 
@@ -333,10 +352,10 @@ public class Unit implements Visualizable {
 		if(!Heal || recover == 0) return;
 
 		recover--;
-		for(int i = 1; i <= AttackRange; i++)
+		for(int i = 1; i <= getAttackRange(); i++)
 			for(Tile tile: getSurroundings(i))
 				if(tile.hasAlly(ownerPlayer)) {
-					tile.getUnit().Hp = Math.max(tile.getUnit().HpMax, tile.getUnit().Hp + 4);
+					tile.getUnit().setHp(Math.max(tile.getUnit().getHpMax(), tile.getUnit().getHp() + 4));
 					visualizeHeal(tile);
 					movable = 0;
 					attack = 0;
@@ -348,10 +367,10 @@ public class Unit implements Visualizable {
 		if(!Convert || recover == 0) return;
 
 		recover--;
-		for(int i = 1; i <= AttackRange; i++)
+		for(int i = 1; i <= getAttackRange(); i++)
 			for(Tile tile: getSurroundings(i))
 				if(tile.hasEnemy(ownerPlayer)) {
-					Unit traitor = new Unit(ownerPlayer, ownerCity, CharType.WARRIOR);
+					Unit traitor = new Unit(map, ownerPlayer, ownerCity, CharType.WARRIOR);
 					tile.setUnit(traitor);
 					visualizeConvert(tile);
 					movable = 0;
@@ -359,15 +378,52 @@ public class Unit implements Visualizable {
 				}
 	}
 
+	public ArrayList<Action> getActions() {
+		ArrayList<Action> legalActions = new ArrayList<>();
+		int combined = movable << 2 | attack << 1 | recover;
+		switch(combined) {
+			case 7:
+				if(getHp() < getHpMax())
+					legalActions.add(new ActionRecover(this));
+				if(Heal)
+					legalActions.add(new ActionHeal(this));
+				if(Convert)
+					legalActions.add(new ActionConvert(this));
+				for(Tile tile: getMovable())
+					legalActions.add(new ActionMove(this, tile));
+				for(Tile tile: searchEnemy())
+					legalActions.add(new ActionAttack(this, tile.getUnit()));
+				break;
+			case 1:
+				if(Escape)
+					for(Tile tile: getMovable())
+						legalActions.add(new ActionMove(this, tile));
+				break;
+			case 3:
+				if(Dash)
+					for(Tile tile: searchEnemy())
+						legalActions.add(new ActionAttack(this, tile.getUnit()));
+				break;
+		}
+		return legalActions;
+	}
+
 
 	public String toString() {
+		if(shellType != ShellType.NONE) {
+			switch(shellType) {
+				case BOAT:
+					return ownerPlayer.toString() + "BOAT";
+				case SHIP:
+					return ownerPlayer.toString() + "SHIP";
+				case BATTLESHIP:
+					return ownerPlayer.toString() + "BATTLESHIP";
+			}
+		}
+
 		switch(type) {
 			case ARCHER:
 				return ownerPlayer.toString() + "ARCHER";
-			case BATTLESHIP:
-				return ownerPlayer.toString() + "BATTLESHIP";
-			case BOAT:
-				return ownerPlayer.toString() + "BOAT";
 			case CATAPULT:
 				return ownerPlayer.toString() + "CATAPULT";
 			case DEFENDER:
@@ -376,8 +432,6 @@ public class Unit implements Visualizable {
 				return ownerPlayer.toString() + "MINDBENDER";
 			case RIDER:
 				return ownerPlayer.toString() + "RIDER";
-			case SHIP:
-				return ownerPlayer.toString() + "SHIP";
 			case SWORDSMAN:
 				return ownerPlayer.toString() + "SWORDSMAN";
 			case WARRIOR:
